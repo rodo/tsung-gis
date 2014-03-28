b97338ba9eb1a31c94b26e563979737ddc714be4
Rodolphe Quiédeville <rodolphe@quiedeville.org>
2014-03-28 17:51:55 +0100

    Add tests on postgis

---
+++ postgis_tests.erl
@@ -0,0 +1,41 @@
+%% -*- coding: utf-8 -*-
+%%
+%% Copyright (c) 2014 Rodolphe Quiédeville <rodolphe@quiedeville.org>
+%%
+%%     This program is free software: you can redistribute it and/or modify
+%%     it under the terms of the GNU General Public License as published by
+%%     the Free Software Foundation, either version 3 of the License, or
+%%     (at your option) any later version.
+%%
+%%     This program is distributed in the hope that it will be useful,
+%%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%     GNU General Public License for more details.
+%%
+%%     You should have received a copy of the GNU General Public License
+%%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%%
+%% Unit tests for module : postgis
+
+-module(postgis_tests).
+-include_lib("eunit/include/eunit.hrl").
+-author({author, "Rodolphe Quiédeville", "<rodolphe@quiedeville.org>"}).
+
+rnd_point_test()->
+    Url = postgis:rnd_point(),
+    ?assertEqual(true, is_list(Url)).
+
+rnd_point_tsung_test()->
+    % Tsung call
+    Url = postgis:rnd_point({4,4}),
+    ?assertEqual(true, is_list(Url)).
+
+rnd_box2d_test()->
+    Url = postgis:rnd_box2d(),
+    ?assertEqual(true, is_list(Url)).
+
+rnd_box2d_tsung_test()->
+    % Tsung call
+    Url = postgis:rnd_box2d({4,4}),
+    ?assertEqual(true, is_list(Url)).
+
